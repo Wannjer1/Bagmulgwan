dc5e7e7694e2dbf9b6fb9f06d62ef33cf55ebdf4
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-30 16:57:14 +0300

    bg-bug fix gallery path

--- gallery_app/urls.py
+++ gallery_app/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import path 
+from django.urls import path 
 from gallery_app.views import landingpage
 
 urlpatterns = [
-    path('', landingpage, name='home'),
+    path('gallery/', landingpage, name="landingpage"),
 ]
