9b66fdf38374dc18a713d6bce753d82795aba4c0
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-31 00:04:38 +0300

    added a location class

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -23,3 +23,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+# location model
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
