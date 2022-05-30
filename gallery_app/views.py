13bffe04bc133f2f6b6a59cd6ea914f12480d0af
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-30 14:31:45 +0300

    created view to the landing page

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def landingpage(request):
+    html = '<h1>Home</h1>'
+    return HttpResponse(html)
