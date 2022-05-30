79d5c588fa7c9bf17d50b3201682fc92ceef9e43
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-31 00:29:39 +0300

    added location and category class and created a relationship with images model class

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,10 +1,56 @@
+
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image = models.ImageField()
+class Images(models.Model):
+    image_link = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length=100)
     description = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, default=1)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE, default=1)
+
+
+    def save_image(self):
+        '''Method to save the image'''
+        self.save()
+
+    def delete_image(self):
+        '''Method to delete the image'''
+        self.delete()
+
+    @classmethod
+    def get_all(cls):
+        '''
+        method to retrieve all images
+        '''
+        pics = Images.objects.all()
+        return pics
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        '''
+        method to retrieve images by unique id
+        '''
+        retrieved = Images.objects.get(id = id)
+        return retrieved
+
+    @classmethod
+    def search_image(cls, cat):
+        '''
+        method to search images by category
+        '''
+        retrieved = cls.objects.filter(category__name__contains=cat) #images assoc w/ this cat
+        return retrieved #list of instances
+
+    @classmethod
+    def filter_by_location(cls ,location):
+        '''
+        method to retrive images by their locations
+        '''
+        retrieved = Images.objects.filter(location__city__contains=location)
+        return retrieved
+
+
    
 
 # category model
@@ -12,13 +58,31 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def save_category(self):
+        '''Method to save category'''
         self.save()
 
-    def update_category(self):
-        self.update()
+    def update_category(self, update):
+        '''Method to update category'''
+        self.name = update
+        self.save()
 
     def delete_category(self):
+        '''Method to delete category'''
         self.delete()
+
+    @classmethod
+    def update_category(cls, search_term , new_cat):
+        '''
+        method to update a category
+        '''
+        try:
+            to_update = Category.objects.get(name = search_term)
+            to_update.name = new_cat
+            to_update.save()
+            return to_update
+        except Category.DoesNotExist:
+            print('Category you specified does not exist')
+
 
     def __str__(self):
         return self.name
@@ -30,10 +94,34 @@ class Location(models.Model):
     name = models.CharField(max_length=50)
 
     def save_location(self):
+        '''Method to save the location'''
         self.save()
 
     def delete_location(self):
+        '''Method to delete the location'''
         self.delete()
+
+    @classmethod
+    def update_location(cls, search_term , new_locale):
+        '''
+        method to update a location's city name
+        '''
+        try:
+            to_update = Location.objects.get(country = search_term)
+            to_update.city = new_locale
+            to_update.save()
+            return to_update
+        except Location.DoesNotExist:
+            print('Location you specified does not exist')
+
+    @classmethod
+    def get_all(cls):
+        '''
+        method to retrive all stored locations
+        '''
+        cities = Location.objects.all()
+        return cities
+
 
     def __str__(self):
         return self.name
