a71bb0c62f9bdbc9eb7bc323e97c300563ee7c16
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-31 00:02:44 +0300

    created a category class

--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+   
+
+# category model
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+
+    def update_category(self):
+        self.update()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
