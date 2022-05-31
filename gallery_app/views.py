8eb6f1c3f58fc7e43c7b69c845dc72881bd26a0f
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-31 13:53:41 +0300

    created function to search by category

--- gallery_app/views.py
+++ gallery_app/views.py
@@ -13,6 +13,19 @@ def gallery(request):
    
     return render(request, 'gallery/gallery.html')
 
+# function to enable users to search by category
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        res = Images.search_image(search_term)
+        message = f'{search_term}'
+
+        return render (request, 'gallery/search.html', {'message': message, 'results': res})
+    
+    else:
+        message = 'You have not searched any term'
+        return render(request, 'search.html', {'message':message})
+
 def location(request,locale):
     '''Method to filter images by location'''
     images = Images.filter_by_location(locale)
