6ae050fbce1128f88d8891593af260e960954adf
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-31 13:55:16 +0300

    added path to search by category

--- gallery_app/urls.py
+++ gallery_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path 
-from gallery_app.views import landingpage,gallery,location
+from gallery_app.views import landingpage,gallery,location,search
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('',landingpage, name='home'),
     path('gallery/', gallery, name="gallery"),
+    path('search/', search, name="search"),
     path('location/<str:location>/', location, name='location'),
 ]
 
